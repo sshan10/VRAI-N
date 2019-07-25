0f642aff444531d9d51a930a8cef59fe8a5fcf0b
AIAIYA <39114526+sshan10@users.noreply.github.com>
2019-07-25 22:38:45 +0900

    Add previous works

---
+++ Assets/Scripts/GetVoicePlayWindows.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZenFulcrum.EmbeddedBrowser;
+using HTC.UnityPlugin.Vive;
+
+public class GetVoicePlayWindows : MonoBehaviour
+{
+    public Camera virtualHMD;
+
+    public GameObject browserWindowPrefab;
+
+    public string URL;
+
+    private GameObject browserWindow;
+    private Browser browserComponent;
+    private Animator browserVisualingAnimator;
+
+    Vector3 browserRotation;
+
+    public void Start()
+    {
+        float yRotation = virtualHMD.transform.eulerAngles.y;
+        browserRotation = new Vector3(0, yRotation, 0);
+        browserWindow = null;
+        browserComponent = null;
+        browserVisualingAnimator = null;
+    }
+
+    public void InstantiateBrowserWindow()
+    {
+        if(browserWindow != null)
+        {
+            browserWindow = Instantiate(browserWindowPrefab, virtualHMD.transform.position, Quaternion.Euler(browserRotation));
+            browserComponent = browserWindow.GetComponentInChildren<Browser>();
+            browserComponent.Url = URL;
+        }
+    }
+
+    public void InitializeBrowserWindow()
+    {
+        if(browserVisualingAnimator != null)
+        {
+            StartCoroutine("WaitAndInitialize");
+        }
+    }
+
+    IEnumerator WaitAndInitialize()
+    {
+        browserVisualingAnimator.SetTrigger("Destroy");
+        yield return new WaitForSeconds(1f);
+        Destroy(browserWindow);
+        browserWindow = null;
+        URL = null;
+    }
+
+    void Update()
+    {
+        if (ViveInput.GetPressDownEx(HandRole.LeftHand, ControllerButton.Trigger))
+        {
+            InstantiateBrowserWindow();
+        }
+        if (ViveInput.GetPressDownEx(HandRole.LeftHand, ControllerButton.Grip))
+        {
+            InitializeBrowserWindow();
+        }
+    }
+}
---
+++ Assets/Scripts/OdysseyControllerInput.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using HTC.UnityPlugin.Vive;
+using System.Collections.Generic;
+
+namespace ZenFulcrum.EmbeddedBrowser
+{
+    public class OdysseyControllerInput : MonoBehaviour
+    {
+        public bool Tracked { get; private set; }
+
+        public MouseButton DepressedButtons { get; private set; }
+
+        public Vector2 ScrollDelta { get; private set; }
+
+        public string Name { get; private set; }
+
+        public GameObject browserWindow;
+
+        private LineRenderer visual;
+        private void Start()
+        {
+            visual = GetComponent<LineRenderer>();
+        }
+
+        public virtual void Update()
+        {
+            ReadInput();
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                if (hit.transform.gameObject == browserWindow)
+                {
+                    visual.enabled = true;
+                }
+            }
+        }
+
+        protected virtual void ReadInput()
+        {
+            var leftClick = ViveInput.GetAxis(HandRole.RightHand, ControllerAxis.Trigger);
+            if (leftClick > .9f) DepressedButtons |= MouseButton.Left;
+        }
+    }
+}
+
+
